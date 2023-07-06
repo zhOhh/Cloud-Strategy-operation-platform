c400b8de152f14d77443860c513a7024d46c20df
zhOhh <1299410261@qq.com>
2023-07-06 14:15:16 +0800

    V1.0

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>测试代码1</h1>
+    <h1>测试代码2</h1>
   </div>
 </template>
 
---
+++ src/test/test.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>测试代码2</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
