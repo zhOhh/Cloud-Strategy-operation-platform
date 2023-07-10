94da647e4558aa18749616daa223571635ca689d
zhOhh <1299410261@qq.com>
2023-07-10 14:56:25 +0800

    V1.0

---
+++ src/api/product/trademark/type.ts
@@ -0,0 +1,28 @@
+export interface ResponseData {
+    code: number,
+    message: string,
+    ok: boolean
+}
+
+//已有品牌ts数据类型
+export interface TradeMark {
+    id?: number,
+    tmName: string,
+    logoUrl: string
+}
+
+//包含全部品牌数据ts类型
+export type Records = TradeMark[]
+
+//获取已有全部品牌数据ts类型
+export interface TradeMarkResponseData extends ResponseData {
+    data: {
+        records: Records,
+        total: number,
+        size: number,
+        current: number,
+        searchCount: boolean,
+        pages: number
+
+    }
+}
