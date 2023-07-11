b2cea0112c39361567caf31703099d9b0e47acae
zhOhh <1299410261@qq.com>
2023-07-11 20:58:08 +0800

    V1.0

--- src/api/product/attr/index.ts
+++ src/api/product/attr/index.ts
@@ -1,6 +1,6 @@
 //这里书写属性相关的API文件
 import request from "@/utils/request";
-import type { CategoryResponseData, AttrResponseData } from "./type";
+import type { CategoryResponseData, AttrResponseData, Attr } from "./type";
 //属性管理模块接口地址
 enum API {
     //获取一级分类接口地址
@@ -21,7 +21,13 @@ enum API {
 export const reqC1 = () => request.get<any, CategoryResponseData>(API.C1_URL);
 //获取二级分类的接口方法
 export const reqC2 = (category1Id: number | string) => request.get<any, CategoryResponseData>(API.C2_URL + category1Id);
-//获取三级分类的接口方法
+//获取二级分类的接口方法
 export const reqC3 = (category2Id: number | string) => request.get<any, CategoryResponseData>(API.C3_URL + category2Id);
-// 获取分类下已有的属性与属性值接口
-export const reqAttr = (category1Id: string | number, category2Id: string | number, category3Id: string | number) => request.get<any, AttrResponseData>(API.ATTR_URL + `${category1Id}/${category2Id}/${category3Id}`)
+//获取对应分类下已有的属性与属性值接口
+export const reqAttr = (category1Id: string | number, category2Id: string | number, category3Id: string | number) => request.get<any, AttrResponseData>(API.ATTR_URL + `${category1Id}/${category2Id}/${category3Id}`);
+
+//新增或者修改已有的属性接口
+export const reqAddOrUpdateAttr = (data: Attr) => request.post<any, any>(API.ADDORUPDATEATTR_URL, data);
+
+//删除某一个已有的属性业务
+export const reqRemoveAttr = (attrId: number) => request.delete<any, any>(API.DELETEATTR_URL + attrId)
--- src/components/Category/index.vue
+++ src/components/Category/index.vue
@@ -2,19 +2,19 @@
     <el-card>
         <el-form :inline="true">
             <el-form-item label="一级分类">
-                <el-select v-model="categoryStore.c1Id" @change="handler">
+                <el-select :disabled="scene == 0 ? false : true" v-model="categoryStore.c1Id" @change="handler">
                     <el-option v-for="(c1, index) in categoryStore.c1Arr" :key="c1.id" :label="c1.name"
                         :value="c1.id"></el-option>
                 </el-select>
             </el-form-item>
             <el-form-item label="二级分类">
-                <el-select v-model="categoryStore.c2Id" @change="handler1">
+                <el-select :disabled="scene == 0 ? false : true" v-model="categoryStore.c2Id" @change="handler1">
                     <el-option v-for="(c2, index) in categoryStore.c2Arr" :key="c2.id" :label="c2.name"
                         :value="c2.id"></el-option>
                 </el-select>
             </el-form-item>
             <el-form-item label="三级分类">
-                <el-select v-model="categoryStore.c3Id">
+                <el-select :disabled="scene == 0 ? false : true" v-model="categoryStore.c3Id">
                     <el-option v-for="(c3, index) in categoryStore.c3Arr" :key="c3.id" :label="c3.name"
                         :value="c3.id"></el-option>
                 </el-select>
@@ -54,7 +54,7 @@ const handler1 = () => {
     // 通知仓库获取三级分类数据
     categoryStore.getC3()
 }
-
+defineProps(['scene'])
 </script>
 
 <style scoped></style>
